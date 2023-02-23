89539c8eb820a431aa89b1a817afd43a04c305f0
prawin10-mech <pra6650@gmail.com>
2023-02-23 15:39:51 +0530

    added mealsItems

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,10 +1,12 @@
 import MealsSummary from "./MealsSummary";
 import classes from "./Meals.module.css";
+import AvailableMeals from "./AvailableMeals";
 
 const Meals = () => {
   return (
     <div className={classes.meals}>
       <MealsSummary />
+      <AvailableMeals />
     </div>
   );
 };
